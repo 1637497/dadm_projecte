553543edfb17d9cea7d785784e44a2c5ecd7825c
Helverr <agarciau7@gmail.com>
2024-03-31 13:10:08 +0200

    asjhid

--- co2_emissions.R
+++ co2_emissions.R
@@ -7,7 +7,8 @@ library(mongolite)
 mongo_client <- mongo(db = "co2_emissions", collection = "co2_emissions",  url = "mongodb://localhost")
 
 # Read the CSV file
-data <- read.csv("/Users/marioamadorhurtado/Desktop/CARRERA/3r/2ns/DADM/PROJECTE/owid-co2-data.csv")
+base <- "C:/Users/34619/Desktop/Enginyeria de Dades/3r/2n semestre/Desenvolupament d'Aplicacions de Dades Massives/Projecte DADM"
+data <- read.csv(paste(base, "owid-co2-data.csv", sep = "/"))
 
 # Select the important columns
 data <- data %>% 
@@ -18,7 +19,7 @@ data <- data %>%
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Fusionar taules EXPORTS
-data_exports <- read.csv("/Users/marioamadorhurtado/Desktop/CARRERA/3r/2ns/DADM/PROJECTE/WORLDWIDE/exports/API_NE.EXP.GNFS.CD_DS2_en_csv_v2_3025.csv", sep = ",", header = FALSE)
+data_exports <- read.csv(paste(base, "API_NE.EXP.GNFS.CD_DS2_en_csv_v2_3025.csv", sep  = "/"), sep  = ",", header = FALSE)
 data_exports <- tail(data_exports, -2)
 
 colnames(data_exports) <- c("country", "code", "indicator_name", "indicator_code", 1960:2021)  # Renombrar las columnas
@@ -33,7 +34,7 @@ data <- data[, c(1:3, 5:ncol(data), 4)]
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Fusionar taules GINI
-data_gini <- read.csv("/Users/marioamadorhurtado/Desktop/CARRERA/3r/2ns/DADM/PROJECTE/WORLDWIDE/GINI/API_SI.POV.GINI_DS2_en_csv_v2_16.csv", sep = ",", header = FALSE)
+data_gini <- read.csv(paste(base, "API_SI.POV.GINI_DS2_en_csv_v2_16.csv", sep  = "/"), sep = ",", header = FALSE)
 data_gini <- tail(data_gini, -2)
 head(data_gini)
 
@@ -49,7 +50,7 @@ data <- data[, c(1:3, 5:ncol(data), 4)]
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
 # Fusionar taules LIFE EXPECTANCY
-data_lf <- read.csv("/Users/marioamadorhurtado/Desktop/CARRERA/3r/2ns/DADM/PROJECTE/WORLDWIDE/life_expectancy/API_SP.DYN.LE00.IN_DS2_en_csv_v2_107.csv", sep = ",", header = FALSE)
+data_lf <- read.csv(paste(base,"API_SP.DYN.LE00.IN_DS2_en_csv_v2_107.csv", sep = "/"), sep = ",", header = FALSE)
 data_lf <- tail(data_lf, -2)
 head(data_lf)
 
